997943f21e741886d5c6f523bb4b24d47420d2a0
Syabani Zainon <syabanizainon@Syabanis-MacBook-Pro.local>
2026-01-01 01:38:03 +0800

    fix: AppColors imports for dashboard v2 widgets

--- lib/features/dashboard/presentation/widgets/v2/primary_quick_actions_v2.dart
+++ lib/features/dashboard/presentation/widgets/v2/primary_quick_actions_v2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../../core/theme/app_colors.dart';
+import 'package:pocketbizz/core/theme/app_colors.dart';
 
 class PrimaryQuickActionsV2 extends StatelessWidget {
   final VoidCallback onAddSale;
--- lib/features/dashboard/presentation/widgets/v2/top_products_cards_v2.dart
+++ lib/features/dashboard/presentation/widgets/v2/top_products_cards_v2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../../core/theme/app_colors.dart';
+import 'package:pocketbizz/core/theme/app_colors.dart';
 import '../../../domain/sme_dashboard_v2_models.dart';
 import 'dashboard_v2_format.dart';
 
